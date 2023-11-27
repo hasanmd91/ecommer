310249e343c608eaeeebff943ea447122309e25e
hasanmd91 <hasanmd1991@hotmail.com>
2023-11-27 16:34:55 +0200

    build redux state managment

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,24 @@ import App from './App';
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
 import { theme } from './theme';
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './state';
+import { Provider } from 'react-redux';
+
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
