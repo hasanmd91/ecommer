0c672a7df79c2e0db4f277346fb658949975cb96
hasanmd91 <hasanmd1991@hotmail.com>
2023-11-27 03:26:36 +0200

    build ui structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes></Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
