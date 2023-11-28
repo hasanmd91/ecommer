8b408e87648bee1de2f489b51f5f690a162871a6
hasanmd91 <hasanmd1991@hotmail.com>
2023-11-28 04:26:26 +0200

    build subscribe

--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import MainCarousel from './MainCarousel';
 import ShoppingList from './ShoppingList';
+import Subscribe from './Subscribe';
 
 const Home = () => {
   return (
     <div>
       <MainCarousel />
       <ShoppingList />
+      <Subscribe />
     </div>
   );
 };
