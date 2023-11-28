63e0132b147735fe3cc964e226743c54176b982f
hasanmd91 <hasanmd1991@hotmail.com>
2023-11-28 03:34:09 +0200

    build ShoppingList

--- client/src/scenes/home/Home.jsx
+++ client/src/scenes/home/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import MainCarousel from './MainCarousel';
+import ShoppingList from './ShoppingList';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <MainCarousel />
+      <ShoppingList />
+    </div>
+  );
 };
 
 export default Home;
